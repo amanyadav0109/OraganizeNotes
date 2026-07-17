288e16353fa8c2101ad942aeaef25cae60bd0d44
amanyadav0109 <amanyadav755081@gmail.com>
2026-07-17 19:00:28 +0530

    firebase err

--- Backend/config/firebaseAdmin.js
+++ Backend/config/firebaseAdmin.js
@@ -1,7 +1,18 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Render
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Local
+  const module = await import("./firebase-adminsdk.json", {
+    with: { type: "json" },
+  });
+  serviceAccount = module.default;
+}
 
 initializeApp({
   credential: cert(serviceAccount),
