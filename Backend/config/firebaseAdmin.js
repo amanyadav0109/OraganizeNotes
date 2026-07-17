ea6e02b72c2e821c25bdb73b578a694096b45e4e
amanyadav0109 <amanyadav755081@gmail.com>
2026-07-17 18:22:34 +0530

    fixing agin same bug

--- Backend/config/firebaseAdmin.js
+++ Backend/config/firebaseAdmin.js
@@ -1,16 +1,10 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "./firebase-adminsdk.json" with { type: "json" };
 
-let credentials = serviceAccount;
-
-// On Render use environment variable
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  credentials = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-}
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 initializeApp({
-  credential: cert(credentials),
+  credential: cert(serviceAccount),
 });
 
 export const adminAuth = getAuth();
