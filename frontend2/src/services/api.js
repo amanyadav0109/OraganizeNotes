aa032ec4fc8bf95ac4497b69e1b57965e5355383
amanyadav0109 <amanyadav755081@gmail.com>
2026-07-17 20:53:05 +0530

    add backend url

--- frontend2/src/services/api.js
+++ frontend2/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "http://localhost:5000/api"
+ baseURL: "https://oraganizedata-d5wr.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
